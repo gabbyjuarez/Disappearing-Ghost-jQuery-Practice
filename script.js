3d71dee83e9fc4072799baec2c85e6b251407600
gabbyjuarez <43676160+gabbyjuarez@users.noreply.github.com>
2019-03-05 09:35:14 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#hide-button").click(function(){
+    $("#ghost1").hide();
+});
+$("#show-button").click(function(){
+    $("#ghost1").show();
+});
+
+$(".say-boo").click(
+    function(){
+        $(".").text ("say-boo");
+       
+    }
+    );
